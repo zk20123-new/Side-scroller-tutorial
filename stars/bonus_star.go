3236ab19c1f7bc3a0418ec5173ea4b159b32b663
zk20124 <d3d87a7f326c8@xutaowang.com>
2025-09-03 17:27:07 +0800

    first commit

---
+++ stars/bonus_star.go
@@ -0,0 +1,43 @@
+components {
+  id: "bonus_star"
+  component: "/stars/bonus_star.script"
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"bonus_star\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/stars/stars.atlas\"\n"
+  "}\n"
+  ""
+  scale {
+    x: 1.961133
+    y: 1.938782
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_SPHERE\n"
+  "    position {\n"
+  "      y: -3.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 1\n"
+  "  }\n"
+  "  data: 59.414513\n"
+  "}\n"
+  ""
+}
---
+++ stars/factory.go
@@ -0,0 +1,16 @@
+components {
+  id: "script"
+  component: "/stars/factory.script"
+}
+embedded_components {
+  id: "star_factory"
+  type: "factory"
+  data: "prototype: \"/stars/star.go\"\n"
+  ""
+}
+embedded_components {
+  id: "aa"
+  type: "factory"
+  data: "prototype: \"/stars/bonus_star.go\"\n"
+  ""
+}
